8e9d97ce95ffcb199e2878238965e6f37db5c390
Mavlon2010 <143276310+Mavlon2010@users.noreply.github.com>
2024-12-15 11:28:46 +0500

    changed: apiview class

--- blog/urls.py
+++ blog/urls.py
@@ -2,15 +2,8 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('blogs/', views.list_blogs, name='list-blogs'),
-    path('blog/<int:pk>/detail', views.detail_blog, name='detail-blog'),
-    path('blogs/search/', views.search_blogs, name='search-blogs'),
-    path('blogs/create/', views.blogs_create, name='blogs-create'),
-    path('blogs/update/<int:pk>', views.blogs_update, name='blogs-update'),
-    path('blogs/delete/<int:pk>', views.blogs_delete, name='blogs-delete'),
-
-
-    path('mobile_blogs/', views.list_mobile_blogs, name='list-mobile-blogs'),
-    path('mobile_blogs/<int:pk>/detail', views.detail_mobile_blogs, name='detail-blog'),
-    path('mobile_blogs/search/', views.search_mobile_blogs, name='search-blogs')
+    # cbw
+    path('cbw/blogs/', views.BlogListCreateAPIView.as_view(), name='cbw-blogs'),
+    # path('cbw/blogs/<int:pk>', views.BlogAPIView.as_view(), name='cbw-blogs-detail'),
+    path('cbw/blogs/<int:pk>', views.BlogRetriveUpdateDeleteAPIView.as_view(), name='cbw-blogs-list-create')
 ]
